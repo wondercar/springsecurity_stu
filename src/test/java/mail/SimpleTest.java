0bbc23403616cb4ad0a9ee0619645abc0fddf352
wondercar <wondercar@qq.com>
2023-04-05 15:09:40 +0800

    添加了测试模块SimpleTest

---
+++ src/test/java/mail/SimpleTest.java
@@ -0,0 +1,11 @@
+package mail;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author: wondercar
+ * @since: 2023-04-05
+ */
+@SpringBootTest
+public class SimpleTest {
+}
